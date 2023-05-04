c91610b0ed6240c75131834bbe6e6af14bbb207a
hafizharamadayanti <128132530+hafizharamadayanti@users.noreply.github.com>
2023-05-04 22:10:57 +0700

    Update UTS LAB AP1.cpp

--- UTS LAB AP1.cpp
+++ UTS LAB AP1.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main() 
 {
-    int n, i, j, k;
+    int n, i, j;
 
 cout<<"==========================\n";
 cout<<"PROGRAM POLA BELAH KETUPAT\n";
@@ -19,7 +19,7 @@ n=n-1;
         for (j = i; j <= n; j++) {
             printf("*");
         }
-        for (k = 1; k <= (2 * i-2) ; k++) {
+        for (j = 1; j <= (2 * i-2) ; j++) {
             printf(" ");
         }
         for (j=i;j<=n;j++)
@@ -35,7 +35,7 @@ for (i = n - 1; i >= 0; i--) {
         for (j = n; j > i; j--) {
             printf("*");
         }
-        for (k = 1; k <= 2 * i; k++) {
+        for (j = 1; j <= 2 * i; j++) {
             printf(" ");
         }   
            for (j = i; j <= n-1; j++) {
