a5926b77b9e731f56bcaca352a746a2d8e763d8c
hafizharamadayanti <128132530+hafizharamadayanti@users.noreply.github.com>
2023-05-04 14:36:05 +0700

    Add files via upload

---
+++ uts lab ap1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main() 
+{
+    int n, i, j, k;
+
+cout<<"==========================\n";
+cout<<"PROGRAM POLA BELAH KETUPAT\n";
+cout<<"==========================\n";
+
+    printf("Masukkan angka: ");
+    scanf("%d", &n);
+
+//bagian atas belah ketupat
+    for (i =1; i <= n ; i++) {
+        for (j = i; j <= n; j++) {
+            printf("*");
+        }
+        for (k = 1; k < 2 * i-2 ; k++) {
+            printf(" ");
+        }
+        for (j=i;j<=n;j++)
+       {
+           printf("*");
+       }
+            
+        printf("\n");
+    }
+
+//bagian bawah belah ketupat
+for (i = n - 1; i >= 1; i--) {
+        for (j = n; j > i; j--) {
+            printf("*");
+        }
+        for (k = 1; k < 2 * i; k++) {
+            printf(" ");
+        }   
+           for (j = i; j <= n-1; j++) {
+            printf("*");
+              
+        }
+        printf("\n");{
+       }
+    }    
+    return 0;
+}
